41eaa18d3409b4106c650b81a7250ec149ea5074
Dejan Ivošević <ivosevicdejann@gmail.com>
2023-08-03 12:03:53 +0200

    web api init setup

---
+++ Api.Sample/Api.Sample/Controllers/TestController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Sample.Controllers
+{
+    [Route("api/test")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        [HttpGet("ping")]
+        public IActionResult Ping()
+        {
+            return Ok();
+        }
+    }
+}
